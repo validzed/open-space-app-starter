1a3b076cb6abee3a7c1dba5514ed05fbf6d9b58f
validzed <validtry@gmail.com>
2023-04-29 04:15:28 +0700

    Membuat Shared Action

--- src/states/shared/action.js
+++ src/states/shared/action.js
@@ -1,3 +1,19 @@
-/**
- * @TODO: Define all the actions (creator) that uses a combination of actions from various domain
- */
+import api from '../../utils/api';
+import { receiveTalksActionCreator } from '../talks/action';
+import { receiveUsersActionCreator } from '../users/action';
+
+function asyncPopulateUsersAndTalks() {
+  return async (dispatch) => {
+    try {
+      const talks = await api.getAllTalks();
+      const users = await api.getAllUsers();
+
+      dispatch(receiveTalksActionCreator(talks));
+      dispatch(receiveUsersActionCreator(users));
+    } catch (error) {
+      alert(error.message);
+    }
+  };
+}
+
+export { asyncPopulateUsersAndTalks };
